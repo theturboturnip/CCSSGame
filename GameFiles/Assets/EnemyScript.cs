4e468e6f7e51435b17a40aebdbe2a73d82aef007
watergun127 <samuel@winslowstark.org>
2014-12-07 06:55:31 +0000

    Added Enemy object

---
+++ GameFiles/Assets/EnemyScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+    public Transform explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision c) {
+	     Instantiate(explosion,transform.position,transform.rotation);
+         GameObject.Destroy(gameObject);
+	}
+}
