bfe99684d9427475fb3f709ea2cb0281a4ea7dc3
watergun127 <samuel@winslowstark.org>
2014-12-19 13:28:12 +0000

    Completely rejigged movement system, remove 1st person view, game now handles like a twin-stick shooter

--- GameFiles/Assets/BulletScript.cs
+++ GameFiles/Assets/BulletScript.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class BulletScript : MonoBehaviour {
 	public Transform startExplosion,endExplosion;
 	public float ticks=0;
+    GameObject model;
 	// Use this for initialization
 	void Start () {
-		if(startExplosion!=null)
-		Instantiate (startExplosion, transform.position, transform.rotation);
+         model=transform.GetChild(0).gameObject;
+		 if(startExplosion!=null)
+		 Instantiate(startExplosion, transform.position, transform.rotation);
 	}
 	void End () {
-		if(endExplosion!=null)
-		Instantiate (endExplosion, transform.position, transform.rotation);
-		GameObject.Destroy (gameObject);
+		 if(endExplosion!=null)
+		 Instantiate(endExplosion, transform.position, transform.rotation);
+		 GameObject.Destroy(gameObject);
 	}
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.forward*Time.deltaTime*100f);	
-		ticks += 1;
-		if (ticks >= 500)
-						End ();
+		 transform.Translate(Vector3.forward*Time.deltaTime*100f);	
+		 if(!model.renderer.isVisible)
+            End();
 	}
 	void OnCollisionEnter(Collision collision){
-		End ();
+		 End();
 	}
 }
---
+++ GameFiles/Assets/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+    public Transform model,bullet;
+    public Vector3 bossLocation=Vector3.zero;
+    public float cameraSpeed,playerSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	     transform.position=Vector3.MoveTowards(transform.position,bossLocation,cameraSpeed*Time.deltaTime);
+         Vector3 mouseWorldPos=Input.mousePosition;
+         mouseWorldPos.z=12;
+         mouseWorldPos=Camera.main.ScreenToWorldPoint(mouseWorldPos);
+         model.LookAt(mouseWorldPos);
+         Vector3 movementVector=new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+         print(movementVector);
+         model.position+=movementVector*playerSpeed*Time.deltaTime;
+         if(Input.GetMouseButton(0)){
+            Instantiate(bullet,model.position,model.rotation);
+         }
+    }
+    
+}
