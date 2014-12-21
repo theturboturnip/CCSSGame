699d83d7b05a63265263f59d50e32c15b9462c40
watergun127 <samuel@winslowstark.org>
2014-12-21 19:06:40 +0000

    Added chain reaction: enemies will die if they are close enough to an explosion

--- GameFiles/Assets/EnemyScript.cs
+++ GameFiles/Assets/EnemyScript.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
     public Transform explosion;
+    bool isDead=false;
     ScoreHandlerScript scoreHandler;
 	// Use this for initialization
 	void Start () {
 		scoreHandler=GameObject.Find("Handlers/ScoreHandler").GetComponent<ScoreHandlerScript>();
 	}
 	public void OnCollisionEnter (Collision c) {
-		print("EnemyHit");
-	    Instantiate(explosion,transform.position,transform.rotation);
-	    scoreHandler.EnemyDestroyed(1,0.1f);
-        GameObject.Destroy(gameObject);
+		string name=c.gameObject.name;
+		if(!isDead&&name!="Enemy"){
+			print("Enemy Hit by "+name);
+	    	Instantiate(explosion,transform.position,transform.rotation);
+	    	scoreHandler.EnemyDestroyed(1,0.1f);
+        	GameObject.Destroy(gameObject);
+        	isDead=true;
+    	}	
 	}
 }
