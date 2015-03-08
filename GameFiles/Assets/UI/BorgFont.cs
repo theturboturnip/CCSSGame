4cfaf606451cebfe53e1fac0b59027a641b895f5
watergun127 <samuel@winslowstark.org>
2015-03-08 21:03:12 +0000

    Started to add borg-themed UI elements

---
+++ GameFiles/Assets/UI/BorgFont.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BorgFont : MonoBehaviour {
+	public Color color1,color2;
+	Text t;
+	public Font f;
+	// Use this for initialization
+	void Start () {
+		t=GetComponent<Text>();
+		t.font=f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 		float lerp = Mathf.PingPong(Time.time, 1.0F);
+        t.color = Color.Lerp(color1, color2, lerp);
+  	}
+}
