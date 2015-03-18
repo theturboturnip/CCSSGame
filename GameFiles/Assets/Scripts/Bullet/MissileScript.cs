29512029fb4c23ae9afd1a8e5bceca099b6aaa98
watergun127 <samuel@winslowstark.org>
2015-03-18 17:26:01 +0000

    Added missile script, has bugs.

---
+++ GameFiles/Assets/Scripts/Bullet/MissileScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileScript : MonoBehaviour {
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		Ray r=Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(r,out hit)){
+			target=hit.transform.gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateTowards(target.transform.position,5.0f);
+		transform.Translate(Vector3.forward*Time.deltaTime);
+	}
+}
